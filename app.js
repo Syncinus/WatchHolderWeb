7357875bfc0d0b8b8380171ce663851a2084d45a
Kael <wkaelj@gmail.com>
2023-06-15 11:25:29 -0600

    add video background

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ new fullpage('#fullpage', {
         if (fadeIn.length !== 0)
             tl.fromTo(fadeIn, 0.7, { y: 25, opacity: 0, }, { y: 0, opacity: 100, })
 
+        if (destination.index === 1) {
+            document.getElementById('s2-backgroundvid').play()
+        }
+
         if (destination.index === 4) {
             let title = section.querySelector('h1')
             tl.fromTo(title, 0.8, { y: 25, opacity: 0, }, { y: 0, opacity: 100, })
@@ -23,8 +27,10 @@ new fullpage('#fullpage', {
     }
 })
 
-
 // payment selection
 function payOptionClick(index) {
     window.location.href = (`./pages/order.html?index=${index}`)
 }
+
+// remove watermark (legal)
+document.getElementsByClassName('fp-watermark').item(0).remove()
